a6865239c152306f9b06b61d09b44eab9f937a58
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2023-07-30 20:44:33 +0200

    feat(install script): add auto generated UUID in .env files

--- easy-install.sh
+++ easy-install.sh
@@ -5,4 +5,14 @@ printf "\033[1mCreating .env files...\n\033[0m"
 cp .env.example .env.development
 cp .env.example .env.production
 cp .env.example .env.test
+
+printf "\033[1mFilling .env files unique jwt secret...\n\033[0m"
+# Replace "YOUR JWT SECRET" with the generated UUID in each .env file
+UUID=$(uuidgen)
+awk -v uuid="$UUID" '{gsub(/YOUR JWT SECRET/, uuid)}1' .env.development > .env.development.tmp && mv .env.development.tmp .env.development
+UUID=$(uuidgen)
+awk -v uuid="$UUID" '{gsub(/YOUR JWT SECRET/, uuid)}1' .env.production > .env.production.tmp && mv .env.production.tmp .env.production
+UUID=$(uuidgen)
+awk -v uuid="$UUID" '{gsub(/YOUR JWT SECRET/, uuid)}1' .env.test > .env.test.tmp && mv .env.test.tmp .env.test
+
 printf "\033[1m=============== \033[33mDon't forget to edit the .env files!\033[0m \033[1m===============\n\033[0m"
